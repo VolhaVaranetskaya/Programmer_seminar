b35b1aec85345bdabba20288c261f59aa35f3931
Volha Varanetskaya <olenka_20@tut.by>
2022-09-22 14:24:21 +0300

    Puzzle05

---
+++ Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принмает одно число (N), 
+// а на выходе показывает все целые числа
+//  в промежутке от -N до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = -num;
+
+while (counter <= num)
+{
+    Console.Write(counter++ + " ");
+}
