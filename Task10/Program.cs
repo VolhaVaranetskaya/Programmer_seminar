6e7d8c38f68defe18502694820b76d19310937d5
Volha Varanetskaya <olenka_20@tut.by>
2022-09-25 19:27:23 +0300

    Добавили решение задачи 10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100,1000);
+int ShowSecondDigit(int number)
+{
+    int result1 = number / 10;
+    int result2 = result1 % 10;
+    return result2;
+}
+int result2 = ShowSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа {number} => {result2}");
