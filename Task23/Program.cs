97ab671e6268d29320457bb165e21c09bd622802
Volha Varanetskaya <olenka_20@tut.by>
2022-09-27 19:24:47 +0300

    Добавили решение ДЗ 23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход чило (N)
+// и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int number)
+{
+    int counter = 1;
+    while(counter <= number)
+    {
+        Console.WriteLine($"{counter, 5} | {Math.Pow(counter, 3)} |");
+        counter++;
+    }
+}
+if(num > 0) Cube(num);
+else Console.WriteLine("Введено некорректное значение");
