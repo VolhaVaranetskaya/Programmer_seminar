28f95e199b3611e8af70a9d31253d0da39fd656c
Volha Varanetskaya <olenka_20@tut.by>
2022-09-30 15:10:42 +0300

    Добавили решение задачи 24

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая показывает на вход  
+// число (А) и выдает сумму чисел от 1 до А
+// 7 - 28
+// 4 - 10
+// 8 - 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumResult = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumResult}");
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+      sum = sum + i;  
+    }
+    return sum;
+}
+
