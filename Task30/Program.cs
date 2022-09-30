9eef55b6af13e5da234adcabdeaf8205aa59e67a
Volha Varanetskaya <olenka_20@tut.by>
2022-09-30 22:05:39 +0300

    Добавили решение задачи 30

---
+++ Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая вводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке
+
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+void GetArray(int[] arr)
+{
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array [i] }");
+    }
+    Console.WriteLine();
+}
