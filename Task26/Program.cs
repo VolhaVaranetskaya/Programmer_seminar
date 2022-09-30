82c5f7d47c6d87cf9697ceb8009e4f6f831832ed
Volha Varanetskaya <olenka_20@tut.by>
2022-09-30 17:47:41 +0300

    Добавили решение задачи 26

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+//  и выдает количество цифр в число
+// 456 - 3, 89126 - 5
+
+Console.WriteLine("Введит число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int QuantityNumbers(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        counter++;
+    }
+    return counter;
+}
+int res = QuantityNumbers(Math.Abs(number));
+Console.WriteLine($"Количество цифр числа {number} => {res}");
