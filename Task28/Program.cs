9e82f0a3e5e5c0e1c347068b67c7635453a619b4
Volha Varanetskaya <olenka_20@tut.by>
2022-09-30 18:59:05 +0300

    Добавили решение задачи 28

---
+++ Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимае на вход число N 
+//  выдает произведение чисел от 1 до N (факториал N)
+//  4-24, 5-120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int result = DigitMultiplication(number);
+    Console.WriteLine($"Факториалом числа {number} является {result}");
+}
+else Console.WriteLine("Введено некорректное значение");
+
+int DigitMultiplication(int num)
+{
+    int multiplication = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        multiplication = multiplication * i;
+    }
+    return multiplication;
+}
