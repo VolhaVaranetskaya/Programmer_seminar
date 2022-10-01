65a5de83c237dd96b495e45e91bc6dffa1b81ef9
Volha Varanetskaya <olenka_20@tut.by>
2022-10-01 20:52:13 +0300

    Добаили решеение ДЗ29

---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задает массив из 8 элементов,
+//  заполненный псевдослучайными числами и выводит их на экран
+
+int[] array = new int[8];
+int[] ArrayEight = GetArray(array);
+PrintArray(array);
+
+int[]GetArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+}
