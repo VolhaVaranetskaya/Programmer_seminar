c6d0a586d07f17618cc664be41912ba7486d8138
Volha Varanetskaya <olenka_20@tut.by>
2022-10-01 19:55:05 +0300

    Добавиои решение ДЗ 27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+// 452 - 11 (4+5+2); 82 - 10 (8+2) 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigit(number);
+Console.WriteLine($"Сумма чисел {number} => {result}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while(num > 10)
+    {
+    result = num % 10;
+    sum = sum + result;
+    num = num / 10;
+    }
+    return sum;
+
+    if(number < 10)
+    sum = num;
+}
