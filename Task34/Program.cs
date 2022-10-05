954a42898dada32dc3be0b93f7680cf95ffcee80
Volha Varanetskaya <olenka_20@tut.by>
2022-10-05 21:56:55 +0300

    Добавили начао решеия ДЗ34

---
+++ Task34/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// На
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[]array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]} ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+int[] AmountHonestNumb (int[] array, int min, int max)
+{
+    int counter = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0);
+        counter++;
+    }
+    return counter;
+}
+int[] arr = CreateArrayRndInt(10, 100, 999);
+PrintArray(arr);
+int[] amountHonestNumb = AmountHonestNumb (arr, 100, 1000);
+Console.WriteLine(amountHonestNumb);
+
