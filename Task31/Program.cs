3aac3430b4a4d6dae1415ab31fd5c4f6af84c6fa
Volha Varanetskaya <olenka_20@tut.by>
2022-10-05 11:29:26 +0300

    Решение задачи 31

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -20,8 +20,8 @@ void PrintArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if(i < array.Length - 1) Console.WriteLine($"{array[i]} ");
-        else Console.WriteLine($"{array[i]}");
+        if(i < array.Length - 1) Console.Write($"{array[i]} ");
+        else Console.Write($"{array[i]}");
     }
     Console.Write("]");
 }
