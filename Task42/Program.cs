aeb97c12b9fe8b44ed443ef7563f9e39952544a0
Volha Varanetskaya <olenka_20@tut.by>
2022-10-11 16:38:17 +0300

    Добавили решение задачи 42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите програииу, которая будет преобразовывать десятичное число в двоичное
+// 45 - 101101, 3 - 11, 2 - 10, 3 - 1101
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int binaryNumUnreversed = ConvertNumberToBinary(number);
+Console.WriteLine($"{number} => {binaryNumUnreversed}");
+int ConvertNumberToBinary(int num)
+{
+    int binaryNumUnreversed = default;
+    int count = 1;
+    while (num > 0)
+    {
+        binaryNumUnreversed = binaryNumUnreversed + num % 2 * count;
+        count = count*10; // увеличиваем на порядок (1, 10, 100, 1000, 100000 и тд)
+        num = num / 2;
+    }
+    return binaryNumUnreversed;
+}
