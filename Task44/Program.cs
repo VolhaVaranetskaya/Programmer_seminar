0031854d21cfcbf7ec135bb24863c27342f2660e
Volha Varanetskaya <olenka_20@tut.by>
2022-10-11 16:50:36 +0300

    Добвили решение задачи 44

---
+++ Task44/Program.cs
@@ -0,0 +1,24 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи (это 0 и 1)
+// Если N = 5 => 01123;
+// Если N = 3 => 011;
+// Если N = 7 => 0112358;
+// Числа Фибоначчи - это ряд чисел, в которых число равно сумме двух прдыдущих.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintFibonacci(number);
+
+void PrintFibonacci(int num)
+{
+    int firstNum = 0;
+    int secondNum = 1;
+    Console.Write($"{firstNum} {secondNum} ");
+
+    for (int i = 2; i < num; i++) // так как извесны первые два числа, то начинаем с 2
+    {
+        int nextNum = firstNum + secondNum;
+        Console.Write($"{nextNum}");
+        firstNum = secondNum;
+        secondNum = nextNum;
+    }
+}
