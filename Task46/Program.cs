34400e597981ae7788e9ffa5c567e4daa5717820
Volha Varanetskaya <olenka_20@tut.by>
2022-10-14 11:50:34 +0300

    Добавили решение задачи 46

---
+++ Task46/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив рахмером m*n,
+//  заполненный случайными целыми числами
+// m = 3, n = 4;
+
+// функция создания двумерного массива
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Метод печати двумерного массива
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j]}  ");
+            else Console.Write($"{matrix[i,j]}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9); // 3-кол во строк, 4-кол во столбцов, 1,9 - диапазон чисел
+PrintMatrix(array2D);
