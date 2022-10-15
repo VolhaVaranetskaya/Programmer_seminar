f5285fb99c6b068d7ee461d797874779eb51e2a5
Volha Varanetskaya <olenka_20@tut.by>
2022-10-15 13:46:55 +0300

    Добавили решение задачи 52

---
+++ Task52/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = CreateMatrixRndInt(m, n);
+PrintMatrix(matrix);
+FindAverageColumnMatrix(matrix);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min=0, int max=5)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverageColumnMatrix(int[,] matrix)
+{
+    double averageColumns = 0;
+    double sumColumns = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumColumns += matrix[i, j];
+        }
+        averageColumns = sumColumns / matrix.GetLength(0);
+        Console.Write($"Сумма {j + 1} столбца = {sumColumns} ");
+        Console.WriteLine($" Среднеарифметическое = {averageColumns}");
+        sumColumns = 0;
+    }
+}
+
+
+
