dc34a7479f7aeab6629ad0a7c772f5d329e72e70
Volha Varanetskaya <olenka_20@tut.by>
2022-10-22 00:35:04 +0300

    Добавили решение задачи 67

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt16(Console.ReadLine());
+int sum = SumOfDigits(number);
+Console.WriteLine(sum);
+
+int SumOfDigits(int num) 
+{
+    int sum = num % 10;
+    if(num > 0) sum += SumOfDigits(num / 10);
+    return sum;
+}
